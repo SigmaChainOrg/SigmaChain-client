39caed0689ff440a341b0b0336621b1fc6f06716
Andrea Rodríguez <andreap.rodriguez@ucuenca.edu.ec>
2025-04-01 00:07:16 -0500

    Feat(shadcn-components): Add and style switch, radio button, and checkbox shadcn components

--- src/app/app/request/unpublished-requests/[id]/[activituId]/page.tsx
+++ src/app/app/request/unpublished-requests/[id]/[activituId]/page.tsx
@@ -1,6 +1,9 @@
 "use client";
+import { LabeledCheckbox } from "@/app/components/labeled-checkbox";
 import { LabeledSwitch } from "@/app/components/labeled-switch";
 import { BreadcrumbHeader } from "@/app/components/shadcn/header";
+import { Label } from "@/app/components/shadcn/label";
+import { RadioGroup, RadioGroupItem } from "@/app/components/shadcn/radio-group";
 import { useParams, usePathname } from "next/navigation";
 
 export default function Home() {
@@ -11,6 +14,21 @@ export default function Home() {
       <div className="col-start-1 col-end-13">
         <BreadcrumbHeader estado={true} path={pathName} />
         <LabeledSwitch label="Prueba" side="left" />
+      </div>
+      <div className="col-start-1 col-end-13">
+        <RadioGroup defaultValue="option-one">
+          <div className="flex items-center space-x-2">
+            <RadioGroupItem value="option-one" id="option-one" />
+            <Label htmlFor="option-one">Option One</Label>
+          </div>
+          <div className="flex items-center space-x-2">
+            <RadioGroupItem value="option-two" id="option-two" />
+            <Label htmlFor="option-two">Option Two</Label>
+          </div>
+        </RadioGroup>
+      </div>
+      <div className="col-start-1 col-end-13">
+        <LabeledCheckbox label="Prueba" side="right" />
       </div>
     </>
   );
