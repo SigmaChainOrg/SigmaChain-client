1da6475a782fa18ae48e415f01c443a4d0d7397e
Alberto Soriano <albertosoriano.e@gmail.com>
2025-04-06 16:30:57 -0500

    feat(project): added tanstack-query and global provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import { poppins, raleway } from "@/app/fonts/fonts";
 import "@/app/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { Metadata } from "next";
+
+const queryClient = new QueryClient();
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${poppins.variable} ${raleway.variable}`}>
-        <main className="">{children}</main>
+        <QueryClientProvider client={queryClient}>
+          <main className="">{children}</main>
+        </QueryClientProvider>
       </body>
     </html>
   );
