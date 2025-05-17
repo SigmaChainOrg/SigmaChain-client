ccca243fc9e0c7bf4b4fd16195c7f434db800a57
Andrea Rodríguez <andreap.rodriguez@ucuenca.edu.ec>
2025-05-16 23:28:26 -0500

    feat(Request-process): Add request-process UI

---
+++ src/app/app/request/state/request-process-store.ts
@@ -0,0 +1,61 @@
+import { create } from "zustand";
+import { immer } from "zustand/middleware/immer";
+
+export interface ActivityField {
+  id: string;
+  order: number;
+  name: string;
+  description: string;
+  type: string;
+  value?: string[];
+  options?: string[];
+  optional: boolean;
+  error: { value?: string };
+}
+
+export interface ActivitySchema {
+  id: string;
+  order: number;
+  name: string;
+  isComplete: boolean;
+  fields: ActivityField[];
+}
+
+export interface RequestProcessState {
+  activities: ActivitySchema[];
+  setIsCompleteActivity: (activityOrder: number, value: boolean) => void;
+  setActivityFieldValue: (activityOrder: number, fieldOrder: number, value: string[]) => void;
+  setActivityFieldError: (activityOrder: number, fieldOrder: number, error: string) => void;
+}
+
+export const useRequestProcessStore = create(
+  immer<RequestProcessState>((set) => ({
+    activities: [
+      {
+        id: "a1",
+        order: 1,
+        name: "string",
+        isComplete: false,
+        fields: [],
+      },
+    ],
+
+    setIsCompleteActivity: (activityOrder, value) =>
+      set((state) => {
+        const activity = state.activities.find((a) => a.order === activityOrder);
+        if (activity) {
+          activity.isComplete = value;
+        }
+      }),
+
+    setActivityFieldValue: (activityOrder, fieldOrder, value) =>
+      set((state) => {
+        const activity = state.activities.find((a) => a.order === activityOrder);
+      }),
+
+    setActivityFieldError: (activityOrder, fieldOrder, error) =>
+      set((state) => {
+        const activity = state.activities.find((a) => a.order === activityOrder);
+      }),
+  })),
+);
