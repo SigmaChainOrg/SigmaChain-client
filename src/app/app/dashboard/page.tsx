66471619854ec5c664c9f993246a1eb4f8c0a340
Alberto Soriano <albertosoriano.e@gmail.com>
2025-08-22 20:37:11 -0500

    fix(dashboard): changed request button condition

--- src/app/app/dashboard/page.tsx
+++ src/app/app/dashboard/page.tsx
@@ -4,13 +4,13 @@ import { Button } from "@/app/components/shadcn/button";
 import { DashboardHeader } from "@/app/components/shadcn/header";
 import { routes } from "@/app/routes";
 import { useGetMe } from "@/features/auth/hooks/use-get-me";
-import { useRouter } from "next/navigation";
 import { useGetRequestPatterns } from "@/features/request-pattern/hooks/use-get-request-patterns";
+import { useRouter } from "next/navigation";
 import { useMemo } from "react";
 
 export default function Home() {
   const router = useRouter();
-  
+
   const { data: userData } = useGetMe({
     includeUserInfo: true,
     includeGroups: true,
@@ -40,7 +40,8 @@ export default function Home() {
       <h1 className="col-start-1 col-end-13 h-auto text-h1">
         Bienvenido {userData?.userInfo?.firstName + " " + userData?.userInfo?.lastName}
       </h1>
-      {unpublishedRequests?.length ? (
+
+      {(unpublishedRequests?.length ?? 0) === 0 && (
         <Button
           className="col-start-7 col-end-9"
           onClick={() => {
@@ -50,6 +51,7 @@ export default function Home() {
           Crear nueva solicitud
         </Button>
       )}
+
       <RequestCardList
         variant="unpublished"
         requests={unpublishedRequests || []}
